decd6451c8df48b1425e240765e857c461e203f5
Darknught <hassino66@yahoo.com>
2023-10-31 18:04:46 +0000

    code

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for hy in str:
+        if ord(hy) <= 97 and ord(hy) >= 122:
+            hy = chr(ord(hy) - 32)
+            print("{:s}".format(hy), end='')
+        print('\n', end="")
