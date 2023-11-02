8d1aaa4759180c87af90df08f6967749f76031bd
Darknught <hassino66@yahoo.com>
2023-11-02 18:31:51 +0000

    code

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
 
+# Program that imports all functions from calculator_1 and handles operations
+
 if __name__ == "__main__":
-    import sys
     from calculator_1 import add, sub, mul, div
+    import sys
 
-    mycount = len(sys.argv) - 1
-    a = int(sys.argv[1])
-    b = int(sys.argv[3]
-    op = {"+": add, "-": sub, "*": mul, "/": div}
-
-    if mycount != 3:
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    if sys.argv[2] not in list(op.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
 
-    print("{} {} {} = {}".format(a, sys.argv[2], b, op[sys.argv[2]](a, b)))
+    # The keys function checks the list if the operator dosnt exist
+    # ops[sys.argv[2]](a, b) calls function that corresponds to op symbol
+    # that the user wants to use
+
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
