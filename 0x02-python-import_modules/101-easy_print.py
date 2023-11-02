42c1902e0588438c93a184c650470ba91ae183e9
Darknught <hassino66@yahoo.com>
2023-11-02 18:51:28 +0000

    code

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,8 +1,2 @@
 #!/usr/bin/python3
 __import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
-
-# Importing the os module.
-# Calling the write() function on the os module.
-# Passing the file descriptor 1 and the string #pythoniscool\n encoded
-# in UTF-8 to the write() function.
-# the encode function encodes a string to a byte string in UTF-8 encoding
