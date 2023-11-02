eb330be1d1f590353ea0a3da9498ab330b5afd6e
Darknught <hassino66@yahoo.com>
2023-11-02 11:56:35 +0000

    code

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-from add_0 import add
+if __name__ == "__main__":
 
-a = 1
-b = 2
+    #this block allows you to write code that can be both run as a sript and imported as module
+    #the __name__ contains the name of the variable  and if set to __main__ then its been run as script
+    #otherwise the current module is imported as module
 
-print("{0} + {1} = {2}".format(a, b, add(a, b)))
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{0} + {1} = {2}".format(a, b, add(a, b)))
