3c713a9566917c626b651bce06312294b843def6
Darknught <hassino66@yahoo.com>
2023-11-04 09:33:47 +0000

    code

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -19,7 +19,7 @@ def replace_in_list(my_list, idx, element):
         if idx out of range (> len(my_list))
     """
 
-    if idx >= 0 and idx <= (len(my_list) - 1):
+    if idx < 0 and idx >= (len(my_list) - 1):
         return my_list
     my_list[idx] = element
     return my_list
