137d2faceceb949eaf184023df6aea934af3b48e
Darknught <hassino66@yahoo.com>
2023-11-06 06:57:28 +0000

    Return a copy of a list divisible by 2 if true

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
+
+    """
+    Finds all multiples of 2 in a list
+    ...
+
+    Parameters
+    ----------
+    my_list : list
+        The list to check
+
+    Return:
+        A new list with True or False, depending
+         on whether the integer at the same position
+         in the original list is a multiple of 2
+    """
     new = my_list.copy()
 
     for i in range(0, len(my_list)):
