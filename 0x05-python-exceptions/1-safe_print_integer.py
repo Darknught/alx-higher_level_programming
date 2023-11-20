bd19456018733abdb4e9fc75e9605bee1142b43f
Darknught <hassino66@yahoo.com>
2023-11-20 15:33:23 +0000

    prints integer with .format

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-    if value == True:
-        return True
-    else:
-        return False
     try:
         print("{:d}".format(value))
-    except ValueError:
-        return value
+        return True
+    except (ValueError, TypeError):
+        return False
