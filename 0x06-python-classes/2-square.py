c0958dcf3fd66a00a89e9f96ce763ebbb8f983ad
Darknught <hassino66@yahoo.com>
2023-11-21 16:54:35 +0000

    Using isinstance for if not an integer

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,14 +6,16 @@ class Square:
     """Represents a square."""
 
     def __init__(self, size=0):
-        """Initializes a new square, Code goes below.
+        """
 
         Args:
             size (int): The size of the new square.
         """
 
-    if not isinstance(size, int):
+    if type(size) is int:
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    else:
         raise TypeError("size must be an integer")
-    elif size < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size
