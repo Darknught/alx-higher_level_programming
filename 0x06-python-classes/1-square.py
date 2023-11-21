8e97ae5663930f85abba1e2d04d9e2afaca7e7ec
Darknught <hassino66@yahoo.com>
2023-11-21 16:22:59 +0000

    defines a class of square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a square with size"""
+
+
+class Square:
+    """No body to work with yet"""
+
+    def __init__(self, size):
+        """Initiates a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
