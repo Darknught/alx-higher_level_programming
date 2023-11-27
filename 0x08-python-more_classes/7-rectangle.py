c587eb9a2d380783973a623e48d20c3a4de8b7a2
Darknught <hassino66@yahoo.com>
2023-11-27 19:32:10 +0000

    print symbol

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -57,7 +57,11 @@ class Rectangle:
         """String representation of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join([str(self.print_symbol) * self.__width] * self.__height)
+        return (
+                "\n".join(
+                    [str(self.print_symbol) * self.__width] * self.__height
+                    )
+                )
 
     def __repr__(self):
         """String representation for recreation using eval()"""
