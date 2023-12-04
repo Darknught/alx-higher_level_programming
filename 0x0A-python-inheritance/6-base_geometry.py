5a1b55494679e3319612333fff2c83d6d3a66a9a
Darknught <hassino66@yahoo.com>
2023-12-04 16:52:37 +0000

    code

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that creates a class"""
+
+
+class BaseGeometry:
+
+    def area(self):
+        """public instance method"""
+        raise Exception("area() is not implemented")
