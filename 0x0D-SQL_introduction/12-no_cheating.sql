68bdce131bf9ed0985d954cfcdc00b16327c5713
Darknught <hassino66@yahoo.com>
2024-01-16 17:45:49 +0000

    code

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,4 @@
 -- Script that updates the score of Bob to 10 in second_table
-SELECT `name`
-FROM `second_table`
-UPDATE 'Bob' TO 10
-ORDER BY `score` DESC;
+UPDATE `name` `second_table`
+SET `score` = 10
+WHERE `name` = 'Bob';
