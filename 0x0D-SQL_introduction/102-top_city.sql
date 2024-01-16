05754452b24f865bc5da2a2d0d55a045a793c073
Darknught <hassino66@yahoo.com>
2024-01-16 20:10:05 +0000

    code

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- A script that displays top 3 cities tempearture during july and August ordered by temp
 SELECT `city`, AVG(`value`) AS `max_temp`
 FROM `temperatures`
-WHERE MONTH(date) IN (7, 8)
+WHERE `month` = 7 OR `month` = 8
 GROUP BY `city`
 ORDER BY `max_temp` DESC
 LIMIT 3;
