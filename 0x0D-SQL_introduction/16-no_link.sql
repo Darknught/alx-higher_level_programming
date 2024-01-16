5eccb3fabe03b2baff9863896360d0e9b014e1e6
Darknught <hassino66@yahoo.com>
2024-01-16 18:56:20 +0000

    code

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,7 +1,8 @@
 -- A script that lists all records of the table 
 -- Results are displayed in descending order
 -- Dosnt list rows without a name value
-SELECT * FROM `second_table`
+SELECT `score`, `name`
+FROM `second_table`
 WHERE `name` IS NOT NULL
 ORDER BY `score` DESC;
 
