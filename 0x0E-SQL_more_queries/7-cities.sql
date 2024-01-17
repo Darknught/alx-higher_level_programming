1a28d57e898d6dfd3a6a6008f5c8bb8a788a2bb2
Darknught <hassino66@yahoo.com>
2024-01-17 12:25:51 +0000

    code

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,12 +1,10 @@
--- A script that creates database and a table
--- id INT unique, auto generated, can’t be null and is a primary key
--- state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
+-- Creates the database hbtn_0d_usa with the table cities.
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
-    PRIVATE KEY(`id`),
+    PRIMARY KEY(`id`),
     `id`       INT          NOT NULL AUTO_INCREMENT,
     `state_id` INT          NOT NULL,
     `name`     VARCHAR(256) NOT NULL,
-    FOREIGN KEY (`state_id`)
-    REFRENCES `hbtn_0d_usa`.`state_id`(`id`)
+    FOREIGN KEY(`state_id`)
+    REFERENCES `hbtn_0d_usa`.`states`(`id`)
 );
