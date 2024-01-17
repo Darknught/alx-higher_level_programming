0020d3bfe046edfd29268a1f12e53a3f1f228b66
Darknught <hassino66@yahoo.com>
2024-01-17 12:12:09 +0000

    code

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,10 +3,10 @@
 -- state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
-  PRIVATE KEY(`id`),
-  `id`       INT            NOT NULL AUTO_INCREMENT,
-  `state_id` INT            NOT NULL,
-  `name`     VARCHAR(256)   NOT NULL,
-  FOREIGN KEY (`state_id`)
-  REFRENCES `hbtn_0d_usa`.`state_id`(`id`)
+    PRIVATE KEY(`id`),
+    `id`       INT            NOT NULL AUTO_INCREMENT,
+    `state_id` INT            NOT NULL,
+    `name`     VARCHAR(256)   NOT NULL,
+    FOREIGN KEY (`state_id`)
+    REFRENCES `hbtn_0d_usa`.`state_id`(`id`)
 );
