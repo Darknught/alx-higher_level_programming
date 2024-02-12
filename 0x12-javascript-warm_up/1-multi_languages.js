430675484731abdac9d9c45829987ea2f207c360
Darknught <hassino66@yahoo.com>
2024-02-12 13:16:09 +0000

    code

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
 // A Script that prints 3 lines
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is aamazing');
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
