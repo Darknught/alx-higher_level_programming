cdda72e44a263c6c373ec08042d1955fd4599c11
Darknught <hassino66@yahoo.com>
2024-02-12 13:57:35 +0000

    code

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 // Script that prints command line Arguments
 const { argv } = require('process');
-if (argv.length <= 2) {
+if (argv.length === 2) {
   console.log('No argument');
-} else {
+} else if (argv.length === 3) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
