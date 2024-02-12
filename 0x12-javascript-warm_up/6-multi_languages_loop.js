99cc41a00e4de9b792c182128fedd1df05abbdbd
Darknught <hassino66@yahoo.com>
2024-02-12 15:36:16 +0000

    code

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// A Script that prints 3 lines with newline char but by using array and loop
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const item of myArray) {
+  console.log(item);
+}
