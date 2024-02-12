b9e1c3cbf55927a51d7313d52b15aef7dda7ffb0
Darknught <hassino66@yahoo.com>
2024-02-12 16:34:13 +0000

    code

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -8,8 +8,6 @@ if (!isNaN(parseInt(argv[2]))) {
     for (let i = 0; i < x; i++) {
       console.log('C is fun');
     }
-  } else {
-    console.log('Missing number of occurrences');
   }
 } else {
   console.log('Missing number of occurrences');
