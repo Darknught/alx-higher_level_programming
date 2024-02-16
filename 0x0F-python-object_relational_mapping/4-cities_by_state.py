0ce5b42fbe36bc3feba04a98003f7a6a90a87b6f
Darknught <hassino66@yahoo.com>
2024-02-16 11:06:53 +0000

    code

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -18,7 +18,8 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     # Execute SQL query to select all cities and sort them by id
-    query = "SELECT * FROM `cities` ORDER BY id ASC"
+    query = "SELECT cities.id, cities.name, states.name FROM cities \
+            JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC"
     cursor.execute(query)
 
     # Fetch all rows
