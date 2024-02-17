0f9a4bb96d88b43e1ca115e2e8c3157acf2ebf3a
Darknught <hassino66@yahoo.com>
2024-02-17 11:28:04 +0000

    code

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -15,11 +15,12 @@ if __name__ == "__main__":
     # Create engine to connect to MySQL server
     engine = create_engine(
             "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-                sys.argv[1], sys.argv[2], sys.argv[3])
+                sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
             )
 
-    # Create session
     Base.metadata.create_all(engine)
+
+    # Create session
     Session = sessionmaker(bind=engine)
     session = Session()
 
