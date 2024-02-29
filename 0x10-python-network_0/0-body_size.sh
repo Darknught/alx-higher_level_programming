633d9507fcfceacad2cac00bc20bba93031f5036
Darknught <hassino66@yahoo.com>
2024-02-29 17:08:01 +0000

    code

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,14 +2,5 @@
 # A script that takes in a URL, sends a request to that URL, and displays
 # the size of the body of the response. size is displayed in bytes
 
-if [ $# -eq 0 ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
+curl -s "$1" | wc -c
 
-URL=$1
-
-response=$(curl -s "$URL")
-response_size=$(echo -n "$response" | wc -c)
-
-echo "$response_size"
