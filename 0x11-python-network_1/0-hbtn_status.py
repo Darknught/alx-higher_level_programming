aff886e017aec0281b18d81860b8ff8fed5fa143
Darknught <hassino66@yahoo.com>
2024-03-01 16:36:08 +0000

    code

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
         body = response.read()
         utf8_content = body.decode("utf-8")
 
-    print("- Body response:")
+    print("Body response:")
     print("\t- type: {}".format(type(body)))
     print("\t- content: {}".format(body))
     print("\t- utf8 content: {}".format(utf8_content))
