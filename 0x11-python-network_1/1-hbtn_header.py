f5c394f854e162ee74bb98397edc09af4c19186d
Darknught <hassino66@yahoo.com>
2024-03-01 16:49:50 +0000

    code

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -21,7 +21,4 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     with urllib.request.urlopen(url) as response:
-        if 'X-Request-Id' in response.headers:
-            print(response.headers['X-Request-Id'])
-        else:
-            print("X-Request-Id header not found in response.")
+        print(response.headers.get('X-Request-Id'))
