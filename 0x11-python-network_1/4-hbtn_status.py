7aedecf310b7526e1e43bfb7b18edb12407d78d0
Darknught <hassino66@yahoo.com>
2024-03-01 17:18:09 +0000

    code

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ A script that fetches https://alx-intranet.hbtn.io/status
 """
-import urllib.request
+import requests
 
 
 if __name__ == "__main__":
-    response = request.get('https://alx-intranet.hbtn.io/status')
+    response = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
