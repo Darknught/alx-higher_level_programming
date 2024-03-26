7e818e873ce18f3a40e18a3f048b2d6618f8b2dc
Darknught <hassino66@yahoo.com>
2024-03-26 14:10:22 +0000

    code

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// A script that writes to a file
+const fs = require('fs');
+const filepath = process.argv[2];
+const data = process.argv[3];
+
+fs.writeFile(filepath, data, 'utf8', (err) => {
+  if (err) {
+    console.log('Error writing file:', err);
+  } else {
+    console.log('File written');
+  }
+});
